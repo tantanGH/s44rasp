eb17f54339ecbebead64906e087694412dca1176
tantanGH <tantangh68@gmail.com>
2023-05-27 21:38:31 +0900

    debug

--- src/ym2608_decode.c
+++ src/ym2608_decode.c
@@ -60,50 +60,17 @@ static inline int16_t ym2608_decode(uint8_t code, int16_t* step_size, int16_t la
 
   static const int16_t step_table[] = { 57, 57, 57, 57, 77, 102, 128, 153 };
 
-  int16_t sign = code & 0x08;
   int16_t delta = code & 0x07;
   int16_t ss = *step_size;
-  int32_t diff = (( 1 + ( delta * 2 )) * ss ) / 64;
- 
-  int16_t next_step = step_table[ delta ] * ss / 64;
-  *step_size = (next_step < 127) ? 127 : (next_step > 24576) ? 24576 : next_step;
+  int32_t diff = ss * ( 1 + ( delta * 2 )) / 8;
 
   int16_t estimate = (code & 0x08) ? last_data - diff : last_data + diff;
   estimate = (estimate > 32767) ? 32767 : (estimate < -32768) ? -32768 : estimate;
+ 
+  int16_t next_step = ss * step_table[ delta ] / 64;
+  *step_size = (next_step < 127) ? 127 : (next_step > 24576) ? 24576 : next_step;
 
   return estimate;
-}
-
-//
-//  YM2608 ADPCM encode
-//
-static uint8_t ym2608_encode(int16_t current_data, int16_t last_estimate, int16_t* step_index, int16_t* new_estimate) {
-
-  int16_t ss = step_size[ *step_index ];
-
-  int16_t delta = current_data - last_estimate;
-
-  uint8_t code = 0x00;
-  if (delta < 0) {
-    code = 0x08;          // bit3 = 1
-    delta = -delta;
-  }
-  if (delta >= ss) {
-    code += 0x04;         // bit2 = 1
-    delta -= ss;
-  }
-  if (delta >= (ss>>1)) {
-    code += 0x02;         // bit1 = 1
-    delta -= ss>>1;
-  }
-  if (delta >= (ss>>2)) {
-    code += 0x01;         // bit0 = 1
-  } 
-
-  // need to use decoder to estimate
-  *new_estimate = ym2608_decode(code, step_index, last_estimate);
-
-  return code;
 }
 
 //
@@ -117,11 +84,11 @@ int32_t ym2608_decode_open(YM2608_DECODE_HANDLE* ym2608, int32_t sample_rate, in
   ym2608->channels = channels;
 
   ym2608->step_size = 127;
-  ym2608->step_index = 32;
+  ym2608->step_index = 0;
   ym2608->last_estimate = 0;
 
   ym2608->step_size2 = 127;
-  ym2608->step_index2 = 32;
+  ym2608->step_index2 = 0;
   ym2608->last_estimate2 = 0;
 
   ym2608->adpcm_counter = 0;
@@ -152,37 +119,9 @@ size_t ym2608_decode_exec(YM2608_DECODE_HANDLE* ym2608, int16_t* output_buffer, 
 
   if (ym2608->sample_rate < 44100) {
 
-    while (source_buffer_ofs < source_buffer_len) {
-
-      // up sampling
-      ym2608->resample_counter += ym2608->sample_rate;
-      if (ym2608->resample_counter < ym2608->resample_rate) {
-
-        output_buffer[ output_buffer_ofs ++ ] = ym2608->last_estimate * 16;
-        output_buffer[ output_buffer_ofs ++ ] = ym2608->last_estimate * 16;
-
-      } else {
-
-        uint8_t code;
-        if ((ym2608->adpcm_counter % 2) == 0) {
-          code = source_buffer[ source_buffer_ofs ] & 0x0f;
-        } else {
-          code = (source_buffer[ source_buffer_ofs++ ] >> 4) & 0x0f;
-        }
-        ym2608->adpcm_counter++;
-
-        int16_t step_index = ym2608->step_index;
-        int16_t new_estimate = ym2608_decode(code, &step_index, ym2608->last_estimate);
-        output_buffer[ output_buffer_ofs ++ ] = new_estimate * 16;   // 12bit signed PCM to 16bit signed PCM
-        output_buffer[ output_buffer_ofs ++ ] = new_estimate * 16;   // mono to stereo duplication
-        ym2608->step_index = step_index;
-        ym2608->last_estimate = new_estimate;
-
-        ym2608->resample_counter -= ym2608->resample_rate;
-
-      }
-
-      //printf("source_buffer_ofs=%d, output_buffer_ofs=%d\n", source_buffer_ofs, output_buffer_ofs);
+    if (ym2608->channels == 1) {
+    
+    } else {
 
     }
 
@@ -206,8 +145,6 @@ size_t ym2608_decode_exec(YM2608_DECODE_HANDLE* ym2608, int16_t* output_buffer, 
         output_buffer[ output_buffer_ofs ++ ] = new_estimate;   // mono to stereo duplication
         ym2608->step_size = step_size;
         ym2608->last_estimate = new_estimate;
-
-        ym2608->resample_counter -= ym2608->resample_rate;
 
       }
 
