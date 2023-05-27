7ad7601de6e91ebd0d8ec75c5d03b16dcf5b87c4
tantanGH <tantangh68@gmail.com>
2023-05-27 11:08:18 +0900

    debug

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 
 // alsa
 #include <alsa/asoundlib.h>
-#include <alsa/error.h>
 
 // codec
 #include "adpcm_decode.h"
@@ -174,7 +173,7 @@ int32_t main(int32_t argc, uint8_t* argv[]) {
   }
 
   // set ASLA PCM parameters
-  if ((alsa_rc = snd_pcm_set_params(pcm_handle, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 
+  if ((alsa_rc = snd_pcm_set_params(pcm_handle, SND_PCM_FORMAT_S16_BE, SND_PCM_ACCESS_RW_INTERLEAVED, 
                           pcm_channels, pcm_freq, ALSA_SOFT_RESAMPLE, ALSA_LATENCY)) != 0) {
     printf("error: pcm device setting error. (%s)\n", snd_strerror(alsa_rc));
     goto exit;
@@ -247,13 +246,14 @@ int32_t main(int32_t argc, uint8_t* argv[]) {
   }
 
   size_t fread_len = fread(pcm_buffer, sizeof(int16_t), pcm_buffer_len, fp);
-  uint8_t* pcm_buffer_uint8 = (uint8_t*)pcm_buffer;
-  for (size_t i = 0; i < fread_len; i++) {
-    uint8_t c = pcm_buffer_uint8[ i * 2 + 0 ];
-    pcm_buffer_uint8[ i * 2 + 0 ] = pcm_buffer_uint8[ i * 2 + 1]; 
-    pcm_buffer_uint8[ i * 2 + 1 ] = c;
-  }
-  if ((alsa_rc = snd_pcm_writei(pcm_handle, (const void*)pcm_buffer, fread_len)) != 0) {
+//  uint8_t* pcm_buffer_uint8 = (uint8_t*)pcm_buffer;
+//  for (size_t i = 0; i < fread_len; i++) {
+//    uint8_t c = pcm_buffer_uint8[ i * 2 + 0 ];
+//    pcm_buffer_uint8[ i * 2 + 0 ] = pcm_buffer_uint8[ i * 2 + 1]; 
+//    pcm_buffer_uint8[ i * 2 + 1 ] = c;
+//  }
+  snd_pcm_uframes_t num_frames = fread_len / pcm_channels;
+  if ((alsa_rc = snd_pcm_writei(pcm_handle, (const void*)pcm_buffer, num_frames)) != 0) {
     printf("error: pcm device write error. (%s)\n", snd_strerror(alsa_rc));
     goto exit;
   }
