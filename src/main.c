8c06ac5e1fbd790e0258d6872d7b2b5c92a6d49e
tantanGH <tantangh68@gmail.com>
2023-05-28 08:59:30 +0900

    debug

--- src/main.c
+++ src/main.c
@@ -231,19 +231,19 @@ int32_t main(int32_t argc, uint8_t* argv[]) {
   if (use_oled) {
     static uint8_t mes[128];
     uint8_t* c = strrchr(pcm_file_name, '/');
-    sprintf(mes, "FILE NAME: %s", c != NULL ? c+1 : pcm_file_name);
+    sprintf(mes, "FILE: %s", c != NULL ? c+1 : pcm_file_name);
     oled_ssd1306_print(&ssd1306, 0, 0, mes);
-    sprintf(mes, "DATA SIZE: %d bytes", pcm_data_size);
+    sprintf(mes, "SIZE: %d bytes", pcm_data_size);
     oled_ssd1306_print(&ssd1306, 0, 1, mes);
-    sprintf(mes, "DATA FORMAT: %s", 
+    sprintf(mes, "FORMAT: %s", 
+      input_format == FORMAT_RAW ? "RAW" : 
       input_format == FORMAT_WAV ? "WAV" :
-      input_format == FORMAT_YM2608 ? "ADPCM(YM2608)" :
-      input_format == FORMAT_RAW ? "16bit signed raw PCM (big)" : 
-      "ADPCM(MSM6258V)");
+      input_format == FORMAT_YM2608 ? "YM2608" :
+      "ADPCM");
     oled_ssd1306_print(&ssd1306, 0, 2, mes);
-    sprintf(mes, "PCM FREQ: %d [Hz]", pcm_freq);
+    sprintf(mes, "FREQ: %d [Hz]", pcm_freq);
     oled_ssd1306_print(&ssd1306, 0, 3, mes);
-    sprintf(mes, "PCM CHANNEL: %s", pcm_channels == 1 ? "mono" : "stereo");
+    sprintf(mes, "CHANNEL: %s", pcm_channels == 1 ? "mono" : "stereo");
     oled_ssd1306_print(&ssd1306, 0, 4, mes);
     oled_ssd1306_print(&ssd1306, 0, 5, "L:");
     oled_ssd1306_print(&ssd1306, 0, 6, "R:");
