b11b2312e511d954e9aa05c021dfe677b2369710
tantanGH <tantangh68@gmail.com>
2023-10-07 23:38:21 +0900

    debug

--- libmad-0.15.1b/make.sh
+++ libmad-0.15.1b/make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for c in "bit decoder fixed frame huffman layer3 layer12 stream synth timer version"; do
+for c in bit decoder fixed frame huffman layer3 layer12 stream synth timer version; do
   gcc -O2 -c ${c}.c
 done
 
