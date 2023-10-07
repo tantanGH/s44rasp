59e9a235e0ccee0b63ae7242a7fc3f218412c39c
tantanGH <tantangh68@gmail.com>
2023-10-07 17:01:08 +0900

    debug

--- src/mp3_decode.h
+++ src/mp3_decode.h
@@ -40,15 +40,6 @@ typedef struct {
 
 } MP3_DECODE_HANDLE;
 
-int32_t wav_decode_open(WAV_DECODE_HANDLE* wav, int16_t up_sampling);
-void wav_decode_close(WAV_DECODE_HANDLE* wav);
-int32_t wav_decode_parse_header(WAV_DECODE_HANDLE* wav, FILE* fp);
-size_t wav_decode_exec(WAV_DECODE_HANDLE* wav, int16_t* output_buffer, int16_t* source_buffer, size_t source_buffer_len);
-
-int32_t ym2608_decode_open(YM2608_DECODE_HANDLE* ym2608, int32_t sample_rate, int16_t channels, int16_t up_sampling);
-void ym2608_decode_close(YM2608_DECODE_HANDLE* ym2608);
-size_t ym2608_decode_exec(YM2608_DECODE_HANDLE* ym2608, int16_t* output_buffer, uint8_t* source_buffer, size_t source_buffer_len);
-
 int32_t mp3_decode_open(MP3_DECODE_HANDLE* decode, int16_t up_sampling);
 void mp3_decode_close(MP3_DECODE_HANDLE* decode);
 int32_t mp3_decode_parse_tags(MP3_DECODE_HANDLE* decode, FILE* fp);
